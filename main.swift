71c6f8043e9149ab1dbde4d4934fdfeaca73f340
genie ✨ <>
2022-07-25 21:45:47 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,55 @@
+// "Genevieve"
+// "19"
+// "Genevieve Chin"
+
+// print("Hello, World!") // yay first line
+
+// Variables 
+
+var listName = "Reminders"
+var totalReminders = 5
+var reminder = "Take dogs for a walk"
+listName = "Monday To-Do's"
+
+print(listName)
+
+var favoriteColor = "red"
+var pet = "bolo bao"
+var friend = "Rahael & Jake"
+var goal = "coding qween"
+var hobby = "baking"
+
+// declare constants using let - values cannot be reassigned 
+
+let name = "Genevieve"
+print(name)
+
+// declaring variable with no value
+var potato : String
+potato = "fried"
+print(potato)
+
+// interpolation 
+print("There are \(potato) items on the menu. ")
+
+var first = "Karlie"
+var last = "Kloss"
+
+print ("\(first) \(last)")
+print ("\(first)\(last)")
+
+var remindersLeft = 5
+print("There are \(remindersLeft) left")
+
+// integers and operations 
+var januaryAvg = 12
+var marchAvg = 65
+var novemberAvg = 31
+var julyAvg = 98
+
+var sum = januaryAvg+marchAvg
+print("sum: \(sum)")
+
+sum = januaryAvg+marchAvg*novemberAvg/ julyAvg
+
+print(sum)
